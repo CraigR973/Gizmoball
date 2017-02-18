faeb90aba7e97b22103f076f8b90da33c2c3dc98
Averkis <averkk@live.com>
2017-02-18 17:22:31 +0000

    added line segs to every triangle side

--- GizmoCollisionPrototype/src/model/Triangle.java
+++ GizmoCollisionPrototype/src/model/Triangle.java
@@ -4,8 +4,6 @@ import java.awt.Color;
 import physics.LineSegment;
 
 public class Triangle {
-	private int h;
-	private int w;
 	private int xpos1;
 	private int xpos2;
 	private int xpos3;
@@ -13,58 +11,75 @@ public class Triangle {
 	private int ypos2;
 	private int ypos3;
 	private Color colour;
-	private LineSegment ls;
-	
-	
-	//x1 = top left, x2 = bottom, x3 = top right, y1 = top left, y2 = bottom, y3 = top right
-	public Triangle (int x1, int x2, int x3, int y1, int y2, int y3){
+	private LineSegment ls1;
+	private LineSegment ls2;
+	private LineSegment ls3;
+	private int width;
+	private int height;
+
+	// x1 = top left, x2 = bottom, x3 = top right, y1 = top left, y2 = bottom,
+	// y3 = top right
+	public Triangle(int x1, int y1, int w, int h) {
 		xpos1 = x1;
-		xpos2 = x2;
-		xpos3 = x3;
+		xpos2 = x1 + w;
+		xpos3 = xpos2;
 		ypos1 = y1;
-		ypos2 = y2;
-		ypos3 = y3;
+		ypos2 = y1;
+		ypos3 = ypos2 + h;
+		width = w;
+		height = h;
 		colour = Color.YELLOW;
-		ls = new LineSegment(x1, y1, x2, y2);
+		ls1 = new LineSegment(x1, y1, xpos2, ypos2);
+		ls2 = new LineSegment(xpos2, ypos2, xpos3, ypos3);
+		ls3 = new LineSegment(x1, y1, xpos3, ypos3);
 	}
-	
-	public Color getColour(){
+
+	public Color getColour() {
 		return colour;
 	}
-	
-	public int getHeight(){
-		return h;
+
+	public int getHeight() {
+		return height;
 	}
-	
-	public int getWidth(){
-		return w;
+
+	public int getWidth() {
+		return width;
 	}
-	
-	public int getXpos1(){
+
+	public int getXpos1() {
 		return xpos1;
 	}
-	
-	public int getXpos2(){
+
+	public int getXpos2() {
 		return xpos2;
 	}
-	
-	public int getXpos3(){
+
+	public int getXpos3() {
 		return xpos3;
 	}
-	
-	public int getYpos1(){
+
+	public int getYpos1() {
 		return ypos1;
 	}
-	
-	public int getYpos2(){
+
+	public int getYpos2() {
 		return ypos2;
 	}
-	
-	public int getYpos3(){
+
+	public int getYpos3() {
 		return ypos3;
 	}
-	
-	public LineSegment getLineSeg(){
-		return ls;
+
+	public LineSegment getLineSeg1() {
+		return ls1;
 	}
+
+	public LineSegment getLineSeg2() {
+		return ls2;
+	}
+
+	public LineSegment getLineSeg3() {
+		return ls3;
+	}
+	
 }
